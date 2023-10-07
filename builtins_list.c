fd7c167b969bf09f37cda9acd7732c1fccbd78ae
JIMMY TOLE <136503759+jimmytole1262@users.noreply.github.com>
2023-10-07 20:50:34 +0300

    world

---
+++ builtins_list.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * builtins_list - The search for match execute builtin
+ * @data: struct for program's data
+ * Return: Returns the return of the function executed is there is a match,
+ * otherwise returns -1.
+ **/
+int builtins_list(data_of_program *data)
+{
+	int iterator;
+	builtins options[] = {
+		{"exit", builtin_exit},
+		{"help", builtin_help},
+		{"cd", builtin_cd},
+		{"alias", builtin_alias},
+		{"env", builtin_env},
+		{"setenv", builtin_set_env},
+		{"unsetenv", builtin_unset_env},
+		{NULL, NULL}
+	};
+	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
+	{
+		if (str_compare(options[iterator].builtin, data->command_name, 0))
+		{
+			return (options[iterator].function(data));
+		}
+	}
+	return (-1);
+}
+/**Robert and Abigael simple project*/
